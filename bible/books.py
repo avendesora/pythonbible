620295402e065a8730fbee227ac8c3de65d0a115
Nathan Patton <npatton@gmail.com>
2020-10-05 17:30:10 -0500

    initial commit with books of the Bible enum and regular expressions for searching for scripture references within a string

---
+++ bible/books.py
@@ -0,0 +1,159 @@
+from enum import IntEnum
+from pathlib import Path
+
+
+class Book(IntEnum):
+    GENESIS = 1
+    EXODUS = 2
+    LEVITICUS = 3
+    NUMBERS = 4
+    DEUTERONOMY = 5
+    JOSHUA = 6
+    JUDGES = 7
+    RUTH = 8
+    SAMUEL_1 = 9
+    SAMUEL_2 = 10
+    KINGS_1 = 11
+    KINGS_2 = 12
+    CHRONICLES_1 = 13
+    CHRONICLES_2 = 14
+    EZRA = 15
+    NEHEMIAH = 16
+    ESTHER = 17
+    JOB = 18
+    PSALMS = 19
+    PROVERBS = 20
+    ECCLESIASTES = 21
+    SONG_OF_SONGS = 22
+    ISAIAH = 23
+    JEREMIAH = 24
+    LAMENTATIONS = 25
+    EZEKIEL = 26
+    DANIEL = 27
+    HOSEA = 28
+    JOEL = 29
+    AMOS = 30
+    OBADIAH = 31
+    JONAH = 32
+    MICAH = 33
+    NAHUM = 34
+    HABAKKUK = 35
+    ZEPHANIAH = 36
+    HAGGAI = 37
+    ZECHARIAH = 38
+    MALACHI = 39
+    MATTHEW = 40
+    MARK = 41
+    LUKE = 42
+    JOHN = 43
+    ACTS = 44
+    ROMANS = 45
+    CORINTHIANS_1 = 46
+    CORINTHIANS_2 = 47
+    GALATIANS = 48
+    EPHESIANS = 49
+    PHILIPPIANS = 50
+    COLOSSIANS = 51
+    THESSALONIANS_1 = 52
+    THESSALONIANS_2 = 53
+    TIMOTHY_1 = 54
+    TIMOTHY_2 = 55
+    TITUS = 56
+    PHILEMON = 57
+    HEBREWS = 58
+    JAMES = 59
+    PETER_1 = 60
+    PETER_2 = 61
+    JOHN_1 = 62
+    JOHN_2 = 63
+    JOHN_3 = 64
+    JUDE = 65
+    REVELATION = 66
+    ESDRAS_1 = 67
+    TOBIT = 68
+    WISDOM_OF_SOLOMON = 69
+    ECCLESIASTICUS = 70
+    MACCABEES_1 = 71
+    MACCABEES_2 = 72
+
+    def name(self):
+        return _BOOK_NAMES.get(self)
+
+
+_BOOK_NAMES = {
+    Book.GENESIS: "Genesis",
+    Book.EXODUS: "Exodus",
+    Book.LEVITICUS: "Leviticus",
+    Book.NUMBERS: "Numbers",
+    Book.DEUTERONOMY: "Deuteronomy",
+    Book.JOSHUA: "Joshua",
+    Book.JUDGES: "Judges",
+    Book.RUTH: "Ruth",
+    Book.SAMUEL_1: "1 Samuel",
+    Book.SAMUEL_2: "2 Samuel",
+    Book.KINGS_1: "1 Kings",
+    Book.KINGS_2: "2 Kings",
+    Book.CHRONICLES_1: "1 Chronicles",
+    Book.CHRONICLES_2: "2 Chronicles",
+    Book.EZRA: "Ezra",
+    Book.NEHEMIAH: "Nehemiah",
+    Book.ESTHER: "Esther",
+    Book.JOB: "Job",
+    Book.PSALMS: "Psalms",
+    Book.PROVERBS: "Proverbs",
+    Book.ECCLESIASTES: "Ecclesiastes",
+    Book.SONG_OF_SONGS: "Song of Songs",
+    Book.ISAIAH: "Isaiah",
+    Book.JEREMIAH: "Jeremiah",
+    Book.LAMENTATIONS: "Lamentations",
+    Book.EZEKIEL: "Ezekiel",
+    Book.DANIEL: "Daniel",
+    Book.HOSEA: "Hosea",
+    Book.JOEL: "Joel",
+    Book.AMOS: "Amos",
+    Book.OBADIAH: "Obadiah",
+    Book.JONAH: "Jonah",
+    Book.MICAH: "Micah",
+    Book.NAHUM: "Nahum",
+    Book.HABAKKUK: "Habakkuk",
+    Book.ZEPHANIAH: "Zephaniah",
+    Book.HAGGAI: "Haggai",
+    Book.ZECHARIAH: "Zechariah",
+    Book.MALACHI: "Malachi",
+    Book.MATTHEW: "Matthew",
+    Book.MARK: "Mark",
+    Book.LUKE: "Luke",
+    Book.JOHN: "John",
+    Book.ACTS: "Acts",
+    Book.ROMANS: "Romans",
+    Book.CORINTHIANS_1: "1 Corinthians",
+    Book.CORINTHIANS_2: "2 Corinthians",
+    Book.GALATIANS: "Galatians",
+    Book.EPHESIANS: "Ephesians",
+    Book.PHILIPPIANS: "Philippians",
+    Book.COLOSSIANS: "Colossians",
+    Book.THESSALONIANS_1: "1 Thessalonians",
+    Book.THESSALONIANS_2: "2 Thessalonians",
+    Book.TIMOTHY_1: "1 Timothy",
+    Book.TIMOTHY_2: "2 Timothy",
+    Book.TITUS: "Titus",
+    Book.PHILEMON: "Philemon",
+    Book.HEBREWS: "Hebrews",
+    Book.JAMES: "James",
+    Book.PETER_1: "1 Peter",
+    Book.PETER_2: "2 Peter",
+    Book.JOHN_1: "1 John",
+    Book.JOHN_2: "2 John",
+    Book.JOHN_3: "3 John",
+    Book.JUDE: "Jude",
+    Book.REVELATION: "Revelation",
+    Book.ESDRAS_1: "1 Esdras",
+    Book.TOBIT: "Tobit",
+    Book.WISDOM_OF_SOLOMON: "Wisdom of Solomon",
+    Book.ECCLESIASTICUS: "Ecclesiasticus",
+    Book.MACCABEES_1: "1 Maccabees",
+    Book.MACCABEES_2: "2 Maccabees",
+}
+
+with open(Path(__file__).resolve().parent / "data" / "verse_ids.txt") as verse_ids_file:
+    VERSE_IDS = [int(verse_id) for verse_id in verse_ids_file.readlines()]
---
+++ bible/regular_expressions.py
@@ -0,0 +1,91 @@
+from bible.books import Book
+
+# noinspection SpellCheckingInspection
+BOOK_REGULAR_EXPRESSIONS = {
+    Book.GENESIS: "Gen(?:esis)?",
+    Book.EXODUS: "Exod(?:us)?",
+    Book.LEVITICUS: "Lev(?:iticus)?",
+    Book.NUMBERS: "Num(?:bers)?",
+    Book.DEUTERONOMY: "Deut(?:eronomy)?",
+    Book.JOSHUA: "Josh(?:ua)?",
+    Book.JUDGES: "Judg(?:es)?",
+    Book.RUTH: "Ruth",
+    Book.SAMUEL_1: r"(?:1|I)(?:\s)?Sam(?:uel)?",
+    Book.SAMUEL_2: r"(?:2|II)(?:\s)?Sam(?:uel)?",
+    Book.KINGS_1: r"(?:1|I)(?:\s)?K(?:in)?gs",
+    Book.KINGS_2: r"(?:2|II)(?:\s)?K(?:in)?gs",
+    Book.CHRONICLES_1: r"(?:1|I)(?:\s)?Chr(?:o(?:n(?:icles)?)?)?",
+    Book.CHRONICLES_2: r"(?:2|II)(?:\s)?Chr(?:o(?:n(?:icles)?)?)?",
+    Book.EZRA: "Ezra",
+    Book.NEHEMIAH: "Neh(?:emiah)?",
+    Book.ESTHER: "Esth(?:er)?",
+    Book.JOB: "Job",
+    Book.PSALMS: "Ps(?:a)?(?:lm(?:s)?)?",
+    Book.PROVERBS: "Prov(?:erbs)?",
+    Book.ECCLESIASTES: "Ecc(?:l(?:es(?:iastes)?)?)?",
+    Book.SONG_OF_SONGS: "Song(?: of Sol(?:omon)?)?",
+    Book.ISAIAH: "Isa(?:iah)?",
+    Book.JEREMIAH: "Jer(?:emiah)?",
+    Book.LAMENTATIONS: "Lam(?:entations)?",
+    Book.EZEKIEL: "Ezek(?:iel)?",
+    Book.DANIEL: "Dan(?:iel)?",
+    Book.HOSEA: "Hos(?:ea)?",
+    Book.JOEL: "Joel",
+    Book.AMOS: "Amos",
+    Book.OBADIAH: "Obad(?:iah)?",
+    Book.JONAH: "Jon(?:ah)?",
+    Book.MICAH: "Mic(?:ah)?",
+    Book.NAHUM: "Nah(?:um)?",
+    Book.HABAKKUK: "Hab(?:akkuk)?",
+    Book.ZEPHANIAH: "Zeph(?:aniah)?",
+    Book.HAGGAI: "Hag(?:gai)?",
+    Book.ZECHARIAH: "Zech(?:ariah)?",
+    Book.MALACHI: "Mal(?:achi)?",
+    Book.MATTHEW: "Matt(?:hew)?",
+    Book.MARK: "Mark",
+    Book.LUKE: "Luke",
+    Book.JOHN: r"(?<!(?:1|2|3|I)\s)(?<!(?:1|2|3|I))John",
+    Book.ACTS: "Acts",
+    Book.ROMANS: "Rom(?:ans)?",
+    Book.CORINTHIANS_1: r"(?:1|I)(?:\s)?Cor(?:inthians)?",
+    Book.CORINTHIANS_2: r"(?:2|II)(?:\s)?Cor(?:inthians)?",
+    Book.GALATIANS: "Gal(?:atians)?",
+    Book.EPHESIANS: "Eph(?:esians)?",
+    Book.PHILIPPIANS: "Phil(?:ippians)?",
+    Book.COLOSSIANS: "Col(?:ossians)?",
+    Book.THESSALONIANS_1: r"(?:1|I)(?:\s)?Thess(?:alonians)?",
+    Book.THESSALONIANS_2: r"(?:2|II)(?:\s)?Thess(?:alonians)?",
+    Book.TIMOTHY_1: r"(?:1|I)(?:\s)?Tim(?:othy)?",
+    Book.TIMOTHY_2: r"(?:2|II)(?:\s)?Tim(?:othy)?",
+    Book.TITUS: "Tit(?:us)?",
+    Book.PHILEMON: "Phlm|Phile(?:m(?:on)?)?",
+    Book.HEBREWS: "Heb(?:rews)?",
+    Book.JAMES: "Ja(?:me)?s",
+    Book.PETER_1: r"(?:1|I)(?:\s)?Pet(?:er)?",
+    Book.PETER_2: r"(?:2|II)(?:\s)?Pet(?:er)?",
+    Book.JOHN_1: r"(?:(?:1|I)(?:\s)?)John",
+    Book.JOHN_2: r"(?:(?:2|II)(?:\s)?)John",
+    Book.JOHN_3: r"(?:(?:3|III)(?:\s)?)John",
+    Book.JUDE: "Jude",
+    Book.REVELATION: r"Rev(?:elation)?(?:\sof Jesus Christ)?",
+    # Book.ESDRAS_1: None,
+    # Book.TOBIT: None,
+    # Book.WISDOM_OF_SOLOMON: None,
+    # Book.ECCLESIASTICUS: None,
+    # Book.MACCABEES_1: None,
+    # Book.MACCABEES_2: None,
+}
+
+BOOK_REGEX = "|".join(BOOK_REGULAR_EXPRESSIONS.values())
+CHAPTER_REGEX = r"(\d{1,3})"
+VERSE_REGEX = r"(\d{1,3})"
+CHAPTER_AND_VERSE_REGEX = fr"({CHAPTER_REGEX}(\s*:\s*{VERSE_REGEX})?)"
+RANGE_REGEX = (
+    fr"({CHAPTER_AND_VERSE_REGEX}(\s*-\s*({CHAPTER_REGEX}\s*:\s*)?{VERSE_REGEX})?)"
+)
+ADDITIONAL_REFERENCE_REGEX = fr"(\s*,\s*({RANGE_REGEX}|{VERSE_REGEX}))"
+FULL_CHAPTER_AND_VERSE_REGEX = f"({RANGE_REGEX}({ADDITIONAL_REFERENCE_REGEX})*)"
+
+SCRIPTURE_REFERENCE_REGULAR_EXPRESSION = (
+    fr"({BOOK_REGEX})\s*({FULL_CHAPTER_AND_VERSE_REGEX})?"
+)
