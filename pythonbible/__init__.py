bbb9c641939d4ed4dab34018082e46abdbdb2452
Nathan Patton <npatton@gmail.com>
2020-10-08 21:15:28 -0500

    updated version number

--- pythonbible/__init__.py
+++ pythonbible/__init__.py
@@ -5,7 +5,7 @@ converting verse ids back into normalized references, and formatting
 references as human-readable strings.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .books import Book
 from .converter import (
