0bc1d8a73456ed42f4595c294560b6fbf94288ac
Nathan Patton <npatton@gmail.com>
2020-10-19 17:52:36 -0500

    version number bump

--- pythonbible/__init__.py
+++ pythonbible/__init__.py
@@ -5,7 +5,7 @@ converting verse ids back into normalized references, and formatting
 references as human-readable strings.
 """
 
-__version__ = "0.0.4"
+__version__ = "0.1.0"
 
 from .bible.osis.parser import OSISParser
 from .books import Book
