61f5bc350778cd4ae4b729b042156d45aba667f4
Nathan Patton <npatton@gmail.com>
2021-05-03 10:59:57 -0500

    point doc edit links to main branch

--- pythonbible-docs/docusaurus.config.js
+++ pythonbible-docs/docusaurus.config.js
@@ -91,8 +91,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/avendesora/pythonbible/edit/feature/book-groups/pythonbible-docs/',
-            // 'https://github.com/avendesora/pythonbible/edit/main/pythonbible-docs/',
+            'https://github.com/avendesora/pythonbible/edit/main/pythonbible-docs/',
         },
         blog: {
           showReadingTime: true,
