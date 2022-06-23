046c0f425cc9ce36a1c7a7749f31944217a87be5
Nathan Patton <npatton@gmail.com>
2022-06-23 12:57:31 -0500

    Update conf.py (#54)
    * Update conf.py
    
    * Update __init__.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020 Nathan Patton"
 author = "Nathan Patton"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7"
+release = "0.7.1"
 
 
 # -- General configuration ---------------------------------------------------
