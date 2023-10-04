a9f1e6c5e3a743e4dd4d1c89ff870c12cf31dea0
Nathan Patton <npatton@gmail.com>
2023-10-04 12:03:57 -0500

    attempt to use nox for tests in actions

---
+++ noxfile.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import nox
+
+PYTEST = "pytest"
+COVERAGE = "coverage"
+
+
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+def tests(session: nox.Session) -> None:
+    session.install(PYTEST)
+    session.run(PYTEST)
+    session.notify(COVERAGE)
+
+
+@nox.session(python=["3.11"])
+def coverage(session: nox.Session) -> None:
+    session.install("coverage[toml]", PYTEST)
+    session.run(COVERAGE, "run", "-m", PYTEST)
+    session.run(COVERAGE, "report", "--show-missing")
