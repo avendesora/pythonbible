5e6a74c73e81b9dcbd9e76587569b375d728fd48
Nathan Patton <npatton@gmail.com>
2024-06-04 23:08:47 -0500

    memory usage enhancements

--- pythonbible/bible/__init__.py
+++ pythonbible/bible/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from functools import lru_cache
 from importlib import import_module
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -20,7 +19,6 @@ SHORT_TITLES: dict[Version, dict[Book, str]] = {}
 LONG_TITLES: dict[Version, dict[Book, str]] = {}
 
 
-@lru_cache
 def get_bible(version: Version, bible_type: str) -> Bible:
     """Return the Bible for the given version and format.
 
@@ -34,8 +32,19 @@ def get_bible(version: Version, bible_type: str) -> Bible:
     version_bibles = BIBLES.get(version, {})
 
     if not version_bibles:
-        _init_version(version)
-        version_bibles = BIBLES.get(version, {})
+        # Lazy-loading of Bible files to conserve memory
+        if not _do_version_files_exist(version):
+            raise MissingVerseFileError
+
+        try:
+            version_module = import_module(
+                f".{version.value.lower()}.{bible_type.lower()}_bible",
+                "pythonbible.bible.versions",
+            )
+        except ModuleNotFoundError as e:
+            raise MissingVerseFileError from e
+
+        return version_module.bible
 
     if version_bible := version_bibles.get(bible_type):
         return version_bible
@@ -63,7 +72,6 @@ def add_bible(version: Version, bible_type: str, version_bible: Bible) -> None:
     BIBLES[version][bible_type] = version_bible
 
 
-@lru_cache
 def get_short_title(version: Version, book: Book) -> str:
     """Return the short title for the given book and version.
 
@@ -77,16 +85,20 @@ def get_short_title(version: Version, book: Book) -> str:
     version_short_titles = SHORT_TITLES.get(version, {})
 
     if not version_short_titles:
-        try:
-            _init_version(version)
-            version_short_titles = SHORT_TITLES.get(version, {})
-        except MissingVerseFileError:
+        # Lazy-loading of Bible files to conserve memory
+        if not _do_version_files_exist(version):
             return book.title
+
+        version_module = import_module(
+            f".{version.value.lower()}",
+            "pythonbible.bible.versions",
+        )
+        version_short_titles = version_module.SHORT_TITLES
+        return version_short_titles.get(book, book.title)
 
     return version_short_titles.get(book, book.title)
 
 
-@lru_cache
 def get_long_title(version: Version, book: Book) -> str:
     """Return the long title for the given book and version.
 
@@ -97,14 +109,20 @@ def get_long_title(version: Version, book: Book) -> str:
     :return: The long title for the given book and version
     :rtype: str
     """
+    # Lazy-loading of Bible files to conserve memory
     version_long_titles = LONG_TITLES.get(version, {})
 
     if not version_long_titles:
-        try:
-            _init_version(version)
-            version_long_titles = LONG_TITLES.get(version, {})
-        except MissingVerseFileError:
+        # Lazy-loading of Bible files to conserve memory
+        if not _do_version_files_exist(version):
             return book.title
+
+        version_module = import_module(
+            f".{version.value.lower()}",
+            "pythonbible.bible.versions",
+        )
+        version_long_titles = version_module.LONG_TITLES
+        return version_long_titles.get(book, book.title)
 
     return version_long_titles.get(book, book.title)
 
@@ -112,17 +130,3 @@ def get_long_title(version: Version, book: Book) -> str:
 def _do_version_files_exist(version: Version) -> bool:
     version_folder = CURRENT_FOLDER / "versions" / version.value.lower()
     return version_folder.exists()
-
-
-def _init_version(version: Version) -> None:
-    # Lazy-loading of Bible files to conserve memory
-    if not _do_version_files_exist(version):
-        raise MissingVerseFileError
-
-    version_module = import_module(
-        f".{version.value.lower()}",
-        "pythonbible.bible.versions",
-    )
-    BIBLES[version] = version_module.BIBLES[version]
-    SHORT_TITLES[version] = version_module.SHORT_TITLES
-    LONG_TITLES[version] = version_module.LONG_TITLES
