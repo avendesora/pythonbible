afc53a21f813b01a9ae7abaace513649252d373e
Nathan Patton <npatton@gmail.com>
2025-11-15 23:03:59 -0600

    attempt to fix tests workflow

--- pythonbible/noxfile.py
+++ pythonbible/noxfile.py
@@ -2,20 +2,19 @@ from __future__ import annotations
 
 import nox
 
-PYTEST = "pytest"
-COVERAGE = "coverage"
-
 nox.options.default_venv_backend = "uv"
 
 
 @nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def tests(session: nox.Session) -> None:
-    session.install(PYTEST)
-    session.run(PYTEST)
+    session.run("uv", "sync", "--active")
+    session.run("uv", "sync", "--active", "--group", "test")
+    session.run("uv", "run", "--active", "pytest")
 
 
 @nox.session(python=["3.13"])
 def coverage(session: nox.Session) -> None:
-    session.install("coverage[toml]", PYTEST)
-    session.run(COVERAGE, "run", "-m", PYTEST)
-    session.run(COVERAGE, "report", "--show-missing")
+    session.run("uv", "sync", "--active")
+    session.run("uv", "sync", "--active", "--group", "test")
+    session.run("uv", "run", "--active", "coverage", "run", "-m", "pytest")
+    session.run("uv", "run", "--active", "coverage", "report", "--show-missing")
